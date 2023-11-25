c32ef9b6677e658b5d275fdd46f41b76639c9d8e
Achinda <achindamihiruk@gmail.com>
2023-11-26 00:00:46 +0530

    response codes added to the service methods

--- To-do-app/back-end/src/main/java/lk/ijse/dep11/todo/api/TaskHttpController.java
+++ To-do-app/back-end/src/main/java/lk/ijse/dep11/todo/api/TaskHttpController.java
@@ -1,6 +1,7 @@
 package lk.ijse.dep11.todo.api;
 
 import lk.ijse.dep11.todo.to.TaskTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,23 +11,27 @@ import java.util.Map;
 @RestController
 @RequestMapping("/tasks")
 public class TaskHttpController {
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public TaskTO createTask(@RequestBody String description){
         System.out.println("create task");
         return new TaskTO(1,"Created",true);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping
     public List<TaskTO> getTask(){
         System.out.println("get all task");
         return new ArrayList<>();
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping("/{id}")
     public void editTask(@PathVariable String id, @RequestBody Map<String,String> params){
         System.out.println("edit a task");
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deleteTask(@PathVariable String id){
         System.out.println("delete a task");
