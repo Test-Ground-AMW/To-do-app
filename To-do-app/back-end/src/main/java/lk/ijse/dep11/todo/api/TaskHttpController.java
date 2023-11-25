8f7b09e560e92be645fe4c2536ff0ab941ef3a82
Achinda <achindamihiruk@gmail.com>
2023-11-26 02:23:06 +0530

    establish a connection pool by using Hikari

--- To-do-app/back-end/src/main/java/lk/ijse/dep11/todo/api/TaskHttpController.java
+++ To-do-app/back-end/src/main/java/lk/ijse/dep11/todo/api/TaskHttpController.java
@@ -1,5 +1,7 @@
 package lk.ijse.dep11.todo.api;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import lk.ijse.dep11.todo.to.TaskTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,18 @@ import java.util.Map;
 @RestController
 @RequestMapping("/tasks")
 public class TaskHttpController {
+
+    private HikariDataSource pool;
+    public TaskHttpController() {
+        HikariConfig config = new HikariConfig();
+        config.setUsername("root");
+        config.setPassword("9674");
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/todo_app");
+        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        config.addDataSourceProperty("maximumPoolSize",10);
+        pool = new HikariDataSource(config);
+    }
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(produces = "application/json", consumes = "application/json")
     public TaskTO createTask(@RequestBody TaskTO task){
